94b5f7413b44af06f97ed7ec49e5954212d6c328
Omrajesh K <omrajesh.kthoughtworks.com@Omrajeshs-MacBook-Pro.local>
2023-12-29 12:26:28 +0530

    LC-home | Rajesh | bugfix - product filters bugs fixed.

--- src/components/modules/home/aside.js
+++ src/components/modules/home/aside.js
@@ -1,25 +1,68 @@
 import React, { useState } from "react";
 import classes from "./home.module.css";
-import Checkbox from "../../ui/checkbox/checkbox";
 import PriceRange from "./priceRange";
+import { companies } from "../../../store/data";
+import Input from "../../ui/input/input";
+import { getProductsByCompanies } from "../../../store/productSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Aside = () => {
-  const [checked, setChecked] = useState(0);
+  const dispatch = useDispatch();
+  const { category } = useSelector((state) => state.product);
 
-  const onCheckboxChangeHandler = () => {
+  const [checked, setChecked] = useState(0);
+  const updatedCompanies = category
+    ? companies
+        .map((company) => {
+          if (company.category === +category)
+            return { ...company, isChecked: false };
+        })
+        .filter((x) => x)
+    : companies.map((company) => {
+        return { ...company, isChecked: false };
+      });
+  const [selectedCompanies, setSelectedCompanies] = useState([]);
+
+  const onCheckboxChangeHandler = (id) => {
+    let updatedCompanies = [];
+    const indexToRemove = selectedCompanies.indexOf(id);
+    if (indexToRemove !== -1) {
+      updatedCompanies = selectedCompanies.toSpliced(indexToRemove, 1);
+    } else {
+      updatedCompanies = [...selectedCompanies, id];
+    }
     setChecked(!checked);
+    setSelectedCompanies(updatedCompanies);
+    dispatch(getProductsByCompanies({ companyIds: updatedCompanies }));
   };
+
   return (
     <aside className={`${classes["aside-wrapper"]}`}>
       <PriceRange />
       <hr className="my-10" />
       <div className="my-10 p-2">
         <h6 class="my-5 font-extrabold dark:text-white">filter by company</h6>
-        <Checkbox
-          label="Nokia"
-          checked={checked}
-          onChange={onCheckboxChangeHandler}
-        />
+        {updatedCompanies.map((company) => (
+          <div
+            key={company.id}
+            class="flex items-center mb-4"
+            onChange={() => onCheckboxChangeHandler(company.id)}
+          >
+            <Input
+              type="checkbox"
+              checked={companies.isChecked}
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            {company.name && (
+              <label
+                for="default-checkbox"
+                className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                {company.name}
+              </label>
+            )}
+          </div>
+        ))}
       </div>
       <hr className="my-10" />
     </aside>
--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,31 +1,101 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { products } from "./data";
 
+const getCategoryProducts = (products, category) =>
+  products.filter((product) => product.category === +category);
+
+const getPriceProducts = (products, price) =>
+  products.filter((product) => product.price <= +price);
+
+const getCompanyProducts = (products, company) =>
+  products.filter((product) => company.includes(product.company));
+
+const computeProductList = (state, company, price, category) => {
+  let products = [];
+  if (company.length === 0 && !price && !category) {
+    // when all are empty
+    products = state.products;
+  } else if (company.length === 0 && !price && category) {
+    // only category
+    products = getCategoryProducts(state.products, category);
+  } else if (company.length === 0 && price && !category) {
+    // only price
+    products = getPriceProducts(state.products, price);
+  } else if (company.length > 0 && !price && !category) {
+    // only company
+    products = getCompanyProducts(state.products, company);
+  } else if (company.length === 0 && price && category) {
+    // only category + price
+    products = getPriceProducts(
+      getCategoryProducts(state.products, category),
+      price
+    );
+  } else if (company.length > 0 && !price && category) {
+    // only category + company
+    products = getCompanyProducts(
+      getCategoryProducts(state.products, category),
+      company
+    );
+  } else if (company.length > 0 && price && !category) {
+    // only company + price
+    products = getCompanyProducts(
+      getPriceProducts(state.products, price),
+      company
+    );
+  } else if (company.length > 0 && price && category) {
+    // only category + price + company
+    const categoryProducts = getCategoryProducts(state.products, category);
+    const priceProducts = getPriceProducts(categoryProducts, price);
+    products = getCompanyProducts(priceProducts, company);
+  }
+  return products;
+};
 export const productSlice = createSlice({
   name: "products",
   initialState: {
     products: products,
     updatedProducts: [],
     category: 0,
+    price: 0,
+    company: [],
   },
   reducers: {
     getProductsByCategory: (state, action) => {
       const { categoryId } = action.payload;
-      const updatedProductList = state.products.filter(
-        (product) => product.category === +categoryId
+      let updatedProductList = computeProductList(
+        state,
+        state.company,
+        state.price,
+        categoryId
       );
-      state.updatedProducts = categoryId ? updatedProductList : products;
+
+      state.updatedProducts = updatedProductList;
       state.category = categoryId;
     },
     getProductsByPrice: (state, action) => {
       const { price } = action.payload;
-      const updatedProductList = state.products.filter(
-        (product) =>
-          product.price <= +price && product.category === +state.category
+      let updatedProductList = computeProductList(
+        state,
+        state.company,
+        price,
+        state.category
       );
-      state.updatedProducts = +state.category
-        ? updatedProductList
-        : products.filter((product) => product.price <= +price);
+
+      state.updatedProducts = updatedProductList;
+      state.price = price;
+    },
+    getProductsByCompanies: (state, action) => {
+      const { companyIds } = action.payload;
+
+      let updatedProductList = computeProductList(
+        state,
+        companyIds,
+        state.price,
+        state.category
+      );
+
+      state.updatedProducts = updatedProductList;
+      state.company = companyIds;
     },
     resetProducts: (state) => {
       state.products = products;
@@ -34,7 +104,11 @@ export const productSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const { getProductsByCategory, getProductsByPrice, resetProducts } =
-  productSlice.actions;
+export const {
+  getProductsByCategory,
+  getProductsByPrice,
+  getProductsByCompanies,
+  resetProducts,
+} = productSlice.actions;
 
 export default productSlice.reducer;
